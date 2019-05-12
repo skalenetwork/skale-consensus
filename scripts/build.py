197449d9ac751a1511e9278d555b3c35438e07dd
kladkogex <13399135+kladkogex@users.noreply.github.com>
2019-05-12 16:27:08 +0300

    SKALE-120  Add travis tests - change to python

---
+++ scripts/build.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+#------------------------------------------------------------------------------
+# Bash script to build cpp-ethereum within TravisCI.
+#
+# The documentation for cpp-ethereum is hosted at http://cpp-ethereum.org
+#
+# ------------------------------------------------------------------------------
+# This file is part of cpp-ethereum.
+#
+# cpp-ethereum is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# cpp-ethereum is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with cpp-ethereum.  If not, see <http://www.gnu.org/licenses/>
+#
+# (c) 2016 cpp-ethereum contributors.
+#------------------------------------------------------------------------------
+#
+#    Copyright (C) 2018-2019 SKALE Labs
+#
+#    This file is part of skale-consensus.
+#
+#    skale-consensus is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, eithe    r version 3 of the License, or
+#    (at your option) any later version.
+#
+#    skale-consensus is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with skale-consensus.  If not, see <http://www.gnu.org/licenses/>.
+#
+#    @file  build.sh
+#    @author Stan Kladko
+#    @date 2018
+#
+
+import sys
+import os
+import subprocess
+
+
+os.chdir("..")
+
+print("Directory changed")
+
+subprocess.call(["/usr/bin/cmake", ".",  "-DCMAKE_BUILD_TYPE=" +  sys.argv[1], "-DCOVERAGE=ON"])
+
+subprocess.call(["/usr/bin/make", "-j2"])
+
+
---
+++ scripts/tests.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+#
+#    Copyright (C) 2018-2019 SKALE Labs
+#
+#    This file is part of skale-consensus.
+#
+#    skale-consensus is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    skale-consensus is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with skale-consensus.  If not, see <http://www.gnu.org/licenses/>.
+#
+#    @file  tests.sh
+#    @author Stan Kladko
+#    @date 2019
+#
+
+import os
+import subprocess
+
+
+
+os.chdir("../test/fournodes")
+
+print("Directory changed")
+
+
+os.system("rm  -rf core");
+os.system("rm  -rf /tmp/*.db");
+subprocess.call("../../cmake-build-debug/consensust")
+
+
