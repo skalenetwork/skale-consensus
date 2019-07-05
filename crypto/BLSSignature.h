0e4b587830c03db4e910d4a5ff150c90e1d85a69
a72d9e2bad9edfd58d6c0248c12c953b71d409d2 <13399135+kladkogex@users.noreply.github.com>
2019-07-05 11:13:15 +0300

    SKALE-1257 Cleaning crypto and return functions

--- crypto/BLSSignature.h
+++ crypto/BLSSignature.h
@@ -9,6 +9,7 @@ class BLSSignature {
 protected:
     shared_ptr<libff::alt_bn128_G1> sig;
 public:
+
     BLSSignature(shared_ptr<string> s);
     BLSSignature( const shared_ptr< libff::alt_bn128_G1 >& sig );
     shared_ptr<libff::alt_bn128_G1> getSig() const;
