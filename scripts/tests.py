a627e67ddb76c67fa049ffcd9d081d31867f9dc5
a72d9e2bad9edfd58d6c0248c12c953b71d409d2 <13399135+kladkogex@users.noreply.github.com>
2019-07-15 19:17:07 +0300

    SKALE-1257 Complete consensus misinterprets boundaries

---
+++ scripts/tests.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+#
+#    Copyright (C) 2018-2019 SKALE Labs
+#
+#    This file is part of skale-consensus.
+#
+#   skale-consensus is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as published
+#   by the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   skale-consensus is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with skale-consensus.  If not, see <https://www.gnu.org/licenses/>.
+#
+#    @file  tests.py
+#    @author Stan Kladko
+#    @date 2019
+
+
+import os
+import subprocess
+import sys
+
+
+def unitTest(_consensustExecutive, _testType):
+    os.system("bash -c rm  -f ./core")
+    subprocess.call([_consensustExecutive, _testType])
+
+
+def fullConsensusTest(_test, _consensustExecutive, _testType):
+    testDir = root + "/test/" + _test
+    os.chdir(testDir)
+    os.system("bash -c rm  -rf " + testDir + "/core")
+    os.system("bash -c rm  -rf /tmp/*.db")
+    subprocess.call([_consensustExecutive, _testType])
+
+
+def getConsensustExecutive():
+    consensustExecutive = root + '/consensust'
+    assert(os.path.isfile(consensustExecutive))
+    return consensustExecutive
+
+
+
+assert(len(sys.argv) == 2)
+
+root = sys.argv[1]
+
+print("Starting tests. Build root:" + sys.argv[1])
+
+consensustExecutive = getConsensustExecutive()
+
+unitTest(consensustExecutive, "[tx-serialize]")
+unitTest(consensustExecutive, "[tx-list-serialize]")
+unitTest(consensustExecutive, "[committed-block-serialize]")
+unitTest(consensustExecutive, "")
+
+
+fullConsensusTest("two_out_of_four", consensustExecutive, "[consensus-stuck]")
+fullConsensusTest("onenode", consensustExecutive, "[consensus-basic]")
+fullConsensusTest("twonodes", consensustExecutive, "[consensus-basic]")
+fullConsensusTest("fournodes", consensustExecutive, "[consensus-basic]")
+fullConsensusTest("sixteennodes", consensustExecutive, "[consensus-basic]")
+fullConsensusTest("fournodes_catchup", consensustExecutive, "[consensus-basic]")
+fullConsensusTest("three_out_of_four", consensustExecutive, "[consensus-basic]")
+
+
+
+
+
