2f164bc5056e5a96120160c8c429554452d53d88
kladkogex <13399135+kladkogex@users.noreply.github.com>
2019-10-20 14:39:51 +0300

    Adding Merkle root

--- headers/BlockProposalHeader.h
+++ headers/BlockProposalHeader.h
@@ -34,7 +34,7 @@ class BlockProposalHeader : public AbstractBlockRequestHeader{
     node_id proposerNodeID;
     ptr<string> hash;
 
-    uint64_t partialHashesCount;
+    uint64_t txCount;
     uint64_t  timeStamp = 0;
     uint32_t  timeStampMs = 0;
 
