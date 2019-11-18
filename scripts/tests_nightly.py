300efaea92038d702cfd12db499e946693582521
kladko <13399135+kladkogex@users.noreply.github.com>
2019-11-18 16:53:32 +0200

    Added nightly tests

---
+++ scripts/tests_nightly.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+#
+#    Copyright (C) 2018-2019 SKALE Labs
+#
+#    This file is part of skale-consensus.
+#
+#   skale-consensus is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as published
+#   by the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   skale-consensus is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with skale-consensus.  If not, see <https://www.gnu.org/licenses/>.
+#
+#    @file  tests.py
+#    @author Stan Kladko
+#    @date 2019
+
+
+import os
+import subprocess
+import sys
+
+def run_without_check(_command):
+    print(">" +_command)
+    subprocess.call(_command, shell = True)
+
+
+def run(_command):
+    print(">" +_command)
+    subprocess.check_call(_command, shell = True)
+
+
+def unitTest(_consensustExecutive, _testType):
+    run("rm -f ./core")
+    run(_consensustExecutive + " "  + _testType)
+
+
+def fullConsensusTest(_test, _consensustExecutive, _testType):
+    testDir = root + "/test/" + _test
+    os.chdir(testDir)
+    run ("pwd")
+    run("rm -rf " + testDir + "/core")
+    run("rm -rf /tmp/*.db")
+    run(_consensustExecutive + " "  + _testType)
+
+
+def getConsensustExecutive():
+    run_without_check("cp -f " + root + "/cmake-build-debug/consensust " + root + "/consensust")
+    run_without_check("cp -f " + root + "/build/consensust " + root + "/consensust")
+    consensustExecutive = root + '/consensust'
+    assert(os.path.isfile(consensustExecutive))
+    return consensustExecutive
+
+assert(len(sys.argv) == 2)
+
+root = sys.argv[1]
+
+print("Starting tests. Build root:" + sys.argv[1])
+
+
+run ("ccache -M 20G")
+
+consensustExecutive = getConsensustExecutive()
+
+unitTest(consensustExecutive, "[tx-serialize]")
+unitTest(consensustExecutive, "[tx-list-serialize]")
+unitTest(consensustExecutive, "[committed-block-serialize]")
+
+try:
+    fullConsensusTest("two_out_of_four", consensustExecutive, "[consensus-stuck]")
+except:
+    print "Success"
+
+os.environ["TEST_TIME_MS"] = "100000"
+fullConsensusTest("onenode", consensustExecutive, "[consensus-basic]")
+fullConsensusTest("twonodes", consensustExecutive, "[consensus-basic]")
+fullConsensusTest("fournodes", consensustExecutive, "[consensus-basic]")
+fullConsensusTest("sixteennodes", consensustExecutive, "[consensus-basic]")
+fullConsensusTest("fournodes_catchup", consensustExecutive, "[consensus-basic]")
+fullConsensusTest("three_out_of_four", consensustExecutive, "[consensus-basic]")
+fullConsensusTest("eightnodes", consensustExecutive, "[consensus-finalization-download]");
+fullConsensusTest("sixteennodes", consensustExecutive, "[consensus-finalization-download]");
+
+
+
+
