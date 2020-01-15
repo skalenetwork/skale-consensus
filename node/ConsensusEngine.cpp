d3d42e576ad6ba5aafe6951a861948ce3a6a014d
kladko <13399135+kladkogex@users.noreply.github.com>
2020-01-15 11:28:55 +0200

    Fixed start problem

--- node/ConsensusEngine.cpp
+++ node/ConsensusEngine.cpp
@@ -522,6 +522,7 @@ ConsensusEngine::ConsensusEngine(ConsensusExtFace &_extFace, uint64_t _lastCommi
     try {
 
         init();
+        BinConsensusInstance::initHistory(1);
 
         ASSERT(_lastCommittedBlockTimeStamp < (uint64_t) 2 * MODERN_TIME);
 
