01c82c919fed21c9e2482e6c553ffe909243f6e7
kladko <13399135+kladkogex@users.noreply.github.com>
2020-04-13 19:26:32 +0300

    SKALE-2443-add-submodules

--- scripts/build.py
+++ scripts/build.py
@@ -67,6 +67,11 @@ print("Got TRAVIS_BUILD_TYPE=" + sys.argv[1])
 print("Got TRAVIS_BUILD_DIR=" + sys.argv[2])
 
 
+run("wget https://cmake.org/files/v3.10/cmake-3.10.3-Linux-x86_64.sh")
+run("chmod +x cmake-3.10.3-Linux-x86_64.sh");
+run("./cmake-3.10.3-Linux-x86_64.sh --skip-license")
+run("ln -s ./cmake-3.10.3-Linux-x86_64/bin/cmake /usr/bin/cmake");
+
 cmakeExecutable = subprocess.check_output(["which", "cmake"])
 
 print("Running cmake: " + cmakeExecutable)
