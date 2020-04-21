0cbd5a0741c2df0695303a53855ef9178ed59589
kladko <13399135+kladkogex@users.noreply.github.com>
2020-04-21 13:56:19 +0300

    SKALE-2469

--- scripts/build_deps.sh
+++ scripts/build_deps.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 set -x
 set -e
+cd ../cryptopp;
+make;
+cd ../libzmq;
+cmake . -Bbuild -DCMAKE_BUILD_TYPE=Debug -DBUILD_STATIC_LIBS=ON
+cmake --build build  -- -j$(nproc)
+cd ../jsoncpp
+cmake . -Bbuild -DCMAKE_BUILD_TYPE=Debug -DBUILD_STATIC_LIBS=ON
+cmake --build build  -- -j$(nproc)
+cp -rf include jsoncpp
+cd ../curl
+cmake . -Bbuild -DCMAKE_BUILD_TYPE=Debug -DBUILD_STATIC_LIBS=ON -DBUILD_SHARED_LIBS=OFF
+cmake --build build  -- -j$(nproc)
+cd ../libjson-rpc-cpp
+cmake . -Bbuild -DCMAKE_BUILD_TYPE=Debug -DBUILD_STATIC_LIBS=ON -DCURL_LIBRARY=../curl/build/libcurl-d.a -DCURL_INCLUDE_DIR=../curl/include \
+    -DJSONCPP_INCLUDE_DIR=../jsoncpp -DJSONCPP_LIBRARY=../jsoncpp/build/src/lib_json/libjsoncpp.a
+cmake --build build  -- -j$(nproc) --trace
 cd ../libBLS/deps;
 ./build.sh;
 cd ..;
 cmake . -Bbuild -DCMAKE_BUILD_TYPE=Debug -DBUILD_STATIC_LIBS=ON
 cmake --build build  -- -j$(nproc)
-cd ../cryptopp;
-make;
-cd ../jsoncpp
-cmake . -Bbuild -DCMAKE_BUILD_TYPE=Debug -DBUILD_STATIC_LIBS=ON
-cmake --build build  -- -j$(nproc)
-cd ../curl;                                                                                                                                      
-cmake . -Bbuild -DBUILD_SHARED_LIBS=OFF -DCMAKE_BUILD_TYPE=Debug -DCURL_INCLUDE_DIR=../curl/include -DCURL_LIBRARY=../curl/build/lib/libcurl-d.a -DBUILD_STATIC_LIBS=ON
-cmake --build build  -- -j$(nproc)
-cd ../libzmq;
-cmake . -Bbuild -DCMAKE_BUILD_TYPE=Debug -DBUILD_STATIC_LIBS=ON
-cmake --build build  -- -j$(nproc)
-cd ../libjson-rpc-cpp
-cmake . -Bbuild -DCMAKE_BUILD_TYPE=Debug -DJSONCPP_INCLUDE_DIR=../jsoncpp/include -DJSONCPP_LIBRARY=../jsoncpp/build/src/lib_json/libjsoncpp.a -DBUILD_STATIC_LIBS=ON -DCURL_INCLUDE_DIR=../curl/include -DCURL_LIBRARY=../curl/build/lib/libcurl-d.a 
-cmake --build build  -- -j$(nproc)
+
