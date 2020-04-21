835d3855400f97deea46c9de017290aba416a366
kladko <13399135+kladkogex@users.noreply.github.com>
2020-04-21 15:53:33 +0300

    SKALE-2469

--- scripts/build_deps.sh
+++ scripts/build_deps.sh
@@ -2,7 +2,7 @@
 set -x
 set -e
 cd ../cryptopp;
-make;
+make -j$(nproc);
 cd ../libzmq;
 cmake . -Bbuild -DCMAKE_BUILD_TYPE=Debug -DBUILD_STATIC_LIBS=ON
 cmake --build build  -- -j$(nproc)
@@ -14,7 +14,7 @@ cd ../curl
 cmake . -Bbuild -DCMAKE_BUILD_TYPE=Debug -DBUILD_STATIC_LIBS=ON -DBUILD_SHARED_LIBS=OFF
 cmake --build build  -- -j$(nproc)
 cd ../libjson-rpc-cpp
-cmake . -Bbuild -DCMAKE_BUILD_TYPE=Debug -DBUILD_STATIC_LIBS=ON -DCURL_LIBRARY=../curl/build/libcurl-d.a -DCURL_INCLUDE_DIR=../curl/include \
+cmake . -Bbuild -DCMAKE_BUILD_TYPE=Debug -DBUILD_STATIC_LIBS=ON -D_BUILD_SHARED_LIBS=OFF -DCURL_LIBRARY=../curl/build/lib/libcurl-d.a -DCURL_INCLUDE_DIR=../curl/include \
     -DJSONCPP_INCLUDE_DIR=../jsoncpp -DJSONCPP_LIBRARY=../jsoncpp/build/src/lib_json/libjsoncpp.a
 cmake --build build  -- -j$(nproc) --trace
 cd ../libBLS/deps;
