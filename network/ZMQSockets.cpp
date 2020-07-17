c74ae755c8a5a871c13aa58373deb3b6380e34f6
kladko <13399135+kladkogex@users.noreply.github.com>
2020-07-17 14:44:14 +0300

    bug/SKALE-2912

--- network/ZMQSockets.cpp
+++ network/ZMQSockets.cpp
@@ -57,7 +57,7 @@ void* ZMQSockets::getDestinationSocket( ptr< NodeInfo > _remoteNodeInfo ) {
     LOG(debug, getThreadName() + " zmq debug: requester = " +  to_string((uint64_t )requester));
 
     int timeout = ZMQ_TIMEOUT;
-    int linger= 1000;
+    int linger= 0;
 
     zmq_setsockopt(requester, ZMQ_SNDTIMEO, &timeout, sizeof(int));
     zmq_setsockopt(requester, ZMQ_RCVTIMEO, &timeout, sizeof(int));
@@ -84,7 +84,7 @@ void * ZMQSockets::getReceiveSocket()  {
         LOG(debug, getThreadName() + " zmq debug: receiveSocket = " + to_string((uint64_t)receiveSocket));
 
         int timeout = ZMQ_TIMEOUT;
-        int linger= 1000;
+        int linger= 0;
 
         zmq_setsockopt(receiveSocket, ZMQ_RCVTIMEO, &timeout, sizeof(int));
         zmq_setsockopt(receiveSocket, ZMQ_SNDTIMEO, &timeout, sizeof(int));
