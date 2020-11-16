98c6db20eeb5a4e670e69fa0fb25bc7bbec28203
kladko <13399135+kladkogex@users.noreply.github.com>
2020-11-16 19:47:06 +0200

    SKALE-3553-remove-busy-wait

--- SkaleCommon.h
+++ SkaleCommon.h
@@ -302,7 +302,7 @@ static const uint64_t BLOCK_PROPOSAL_RECEIVE_TIMEOUT_MS = 30000;
 
 static const uint64_t REBROADCAST_TIMEOUT_MS = 120000;
 
-static const uint64_t ZMQ_RECEIVE_RETRY_MS = 5;
+static const uint64_t ZMQ_RECEIVE_RETRY_MS = 10;
 
 extern void setThreadName(std::string const &_n, ConsensusEngine* _engine);
 
