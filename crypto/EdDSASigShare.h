70b7fa85173f39d624abc4ac6145ea7684d53d8b
kladko <13399135+kladkogex@users.noreply.github.com>
2020-12-06 15:21:16 +0200

    SKALE-3306

---
+++ crypto/EdDSASigShare.h
@@ -0,0 +1,50 @@
+/*
+    Copyright (C) 2019 SKALE Labs
+
+    This file is part of skale-consensus.
+
+    skale-consensus is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    skale-consensus is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with skale-consensus.  If not, see <https://www.gnu.org/licenses/>.
+
+    @file EdDSASigShare.h
+    @author Stan Kladko
+    @date 2019
+*/
+
+#ifndef SKALED_MOCKUPSIGSHARE_H
+#define SKALED_MOCKUPSIGSHARE_H
+
+
+#include "BLSSigShare.h"
+#include "ThresholdSigShare.h"
+
+
+class EdDSASigShare : public ThresholdSigShare {
+
+    uint64_t totalSigners;
+    uint64_t requiredSigners;
+
+    string sigShare;
+
+public:
+
+    EdDSASigShare(const string& _sigShare, schain_id _schainID, block_id _blockID, schain_index _signerIndex,
+                   size_t _totalSigners, size_t _requiredSigners);
+
+    virtual string toString();
+
+    virtual ~EdDSASigShare();
+};
+
+
+#endif  // SKALED_EdDSASIGSHARE_H
