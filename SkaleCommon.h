998cdefe74d0a3d12d047669d5e840dc80bc7e54
kladko <13399135+kladkogex@users.noreply.github.com>
2021-02-15 19:32:06 +0200

    SKALE-3888

--- SkaleCommon.h
+++ SkaleCommon.h
@@ -151,7 +151,7 @@ static const uint64_t MAX_CONSENSUS_HISTORY  = 2 * MAX_ACTIVE_CONSENSUSES;
 static const uint64_t SESSION_KEY_CACHE_SIZE  = 2;
 static const uint64_t SESSION_PUBLIC_KEY_CACHE_SIZE  = 16;
 
-static constexpr uint64_t MAX_CATCHUP_DOWNLOAD_BYTES = 4000000;
+static constexpr uint64_t MAX_CATCHUP_DOWNLOAD_BYTES = 16000000;
 
 static constexpr uint64_t PROPOSAL_HASHES_PER_DB = 100000;
 
