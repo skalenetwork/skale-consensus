c1d9196a8f3ac13c24b3395ee0a3114191a4b6d6
kladko <13399135+kladkogex@users.noreply.github.com>
2021-09-22 17:42:02 +0300

    bug/SKALE-4616-oracle-mvp

--- oracle/OracleResponseMessage.cpp
+++ oracle/OracleResponseMessage.cpp
@@ -16,7 +16,7 @@
     You should have received a copy of the GNU Affero General Public License
     along with skale-consensus.  If not, see <https://www.gnu.org/licenses/>.
 
-    @file OracleRequestBroadcastMessage.cpp
+    @file OracleResponseBroadcastMessage.cpp
     @author Stan Kladko
     @date 2018
 */
@@ -30,26 +30,28 @@
 #include "chains/Schain.h"
 #include "protocols/ProtocolKey.h"
 
-#include "OracleAgent.h"
-#include "OracleRequestBroadcastMessage.h"
+#include "protocols/binconsensus/BinConsensusInstance.h"
 
-OracleRequestBroadcastMessage::OracleRequestBroadcastMessage(string& _uri, block_id _blockID,
+#include "OracleAgent.h"
+#include "OracleResponseMessage.h"
+
+OracleResponseMessage::OracleResponseMessage(string& _uri, block_id _blockID,
                                                              uint64_t _timeMs,
                                                              OracleAgent &sourceProtocolInstance)
-        : NetworkMessage(MSG_ORACLE_REQ_BROADCAST, _blockID, 0, 0, 0, _timeMs,
+        : NetworkMessage(MSG_ORACLE_RSP, _blockID, 0, 0, 0, _timeMs,
                          sourceProtocolInstance), uri(_uri) {
     printPrefix = "o";
 }
 
 
-OracleRequestBroadcastMessage::OracleRequestBroadcastMessage(string& _uri, node_id _srcNodeID, block_id _blockID,
+OracleResponseMessage::OracleResponseMessage(string& _uri, node_id _srcNodeID, block_id _blockID,
                                                              uint64_t _timeMs,
                                                              schain_id _schainId, msg_id _msgID,
                                                              schain_index _srcSchainIndex,
                                                              const string &_ecdsaSig, const string &_publicKey,
                                                              const string &_pkSig, Schain *_sChain)
         : NetworkMessage(
-        MSG_ORACLE_REQ_BROADCAST, _srcNodeID, _blockID, 0, 0, 0, _timeMs, _schainId, _msgID,
+        MSG_ORACLE_RSP, _srcNodeID, _blockID, 0, 0, 0, _timeMs, _schainId, _msgID,
         "", _ecdsaSig, _publicKey, _pkSig,
         _srcSchainIndex, _sChain->getCryptoManager()), uri(_uri) {
     printPrefix = "o";
---
+++ oracle/OracleResponseMessage.h
@@ -0,0 +1,43 @@
+/*
+    Copyright (C) 2018-2019 SKALE Labs
+
+    This file is part of skale-consensus.
+
+    skale-consensus is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    skale-consensus is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with skale-consensus.  If not, see <https://www.gnu.org/licenses/>.
+
+    @file OracleResponseBroadcastMessage.h
+    @author Stan Kladko
+    @date 2018
+*/
+
+#pragma once
+
+
+#include "messages/NetworkMessage.h"
+
+class OracleProtocolInstance;
+
+
+class OracleResponseMessage : public NetworkMessage {
+
+    string uri;
+
+public:
+
+    OracleResponseMessage(string& _uri, block_id _blockID, uint64_t _timeMs, OracleAgent& sourceProtocolInstance );
+
+    OracleResponseMessage(string& _uri, node_id _srcNodeID, block_id _blockID,  uint64_t _timeMs, schain_id _schainId,
+                                  msg_id _msgID, schain_index _srcSchainIndex, const string & _ecdsaSig,
+                                  const string & _publicKey, const string & _pkSig, Schain* _sChain );
+};
