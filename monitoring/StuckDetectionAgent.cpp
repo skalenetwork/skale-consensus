cfefc5cfee4ac40481cb00dc9753aded04ff493b
kladko <13399135+kladkogex@users.noreply.github.com>
2021-10-13 15:30:11 +0300

    bug/SKALE-4654

--- monitoring/StuckDetectionAgent.cpp
+++ monitoring/StuckDetectionAgent.cpp
@@ -186,7 +186,6 @@ uint64_t StuckDetectionAgent::checkForRestart( uint64_t _restartIteration ) {
         usleep(5 * 1000 * 1000);
     }
 
-
     LOG( info, "Need for restart detected. Cleaning and restarting " );
     cleanupState();
 
