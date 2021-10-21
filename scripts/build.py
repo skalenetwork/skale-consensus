2b96845694557c34bc01117aa0f89dea9918825d
Oleh Nikolaiev <oleg@skalelabs.com>
2021-10-21 18:15:20 +0300

    SKALE-4693 fix base build

--- scripts/build.py
+++ scripts/build.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 #------------------------------------------------------------------------------
 # Bash script to build cpp-ethereum within TravisCI.
