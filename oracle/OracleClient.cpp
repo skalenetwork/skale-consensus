d4596db55ef249ceed30697d75e6341146d096e5
kladko <13399135+kladkogex@users.noreply.github.com>
2022-01-11 12:02:40 +0200

    bug/SKALE-4616-oracle-mvp

--- oracle/OracleClient.cpp
+++ oracle/OracleClient.cpp
@@ -23,13 +23,13 @@
 
 #include "SkaleCommon.h"
 #include "Log.h"
-#include "OracleRequestBroadcastMessage.h"
 #include "chains/Schain.h"
 #include "messages/MessageEnvelope.h"
 #include "network/Network.h"
 #include "OracleResponseMessage.h"
 #include "utils/Time.h"
 #include "protocols/ProtocolInstance.h"
+#include "OracleReceivedResults.h"
 #include "OracleErrors.h"
 #include "OracleRequestBroadcastMessage.h"
 #include "OracleClient.h"
@@ -92,15 +92,16 @@ void OracleClient::processResponseMessage(const ptr<MessageEnvelope> &_me) {
     CHECK_STATE(origin > 0 || origin <= getSchain()->getNodeCount());
 
     auto receipt = msg->getReceipt();
+    auto res = msg->getOracleResult();
 
-    auto result = receiptsMap.getIfExists(receipt);
+    auto receivedResults = receiptsMap.getIfExists(receipt);
 
-    if (!result.has_value()) {
+    if (!receivedResults.has_value()) {
         LOG(warn, "Received OracleResponseMessage with unknown receipt" + receipt);
         return;
     }
 
-    auto receipts = std::any_cast<ptr<OracleReceivedResults>>(result);
+    auto receipts = std::any_cast<ptr<OracleReceivedResults>>(receivedResults);
 
     LOCK(m)
 
@@ -111,28 +112,38 @@ void OracleClient::processResponseMessage(const ptr<MessageEnvelope> &_me) {
     }
 
 
-    receipts->resultsBySchainIndex->insert({origin, msg->getOracleResult()});
+    receipts->resultsBySchainIndex->insert({origin, res});
+    if (receipts->resultsByCount->count(res) == 0) {
+        receipts->resultsByCount->insert({res, 1});
+    } else {
+        auto count = receipts->resultsByCount->at(res);
+        receipts->resultsByCount->insert({res, count + 1});
+    }
 
     LOG(err, "Processing oracle message:" + to_string(origin));
 }
 
 
 uint64_t OracleClient::tryGettingOracleResult(string& _receipt ,
-                                              string& /* _result */) {
-    auto result = receiptsMap.getIfExists(_receipt);
+                                              string& _result ) {
+    auto oracleReceivedResults = receiptsMap.getIfExists(_receipt);
 
-    if (!result.has_value()) {
+    if (!oracleReceivedResults.has_value()) {
         LOG(warn, "Received tryGettingOracleResult  with unknown receipt" + _receipt);
         return ORACLE_UNKNOWN_RECEIPT;
     }
 
-    auto receipts = std::any_cast<ptr<map<uint64_t, string>>>(result);
 
-    if (receipts->size() < getSchain()->getRequiredSigners()) {
-        return ORACLE_RESULT_NOT_READY;
+    auto receipts = std::any_cast<ptr<OracleReceivedResults>>(oracleReceivedResults);
+
+    for (auto&& item : *receipts->resultsByCount) {
+        if (item.second >= getSchain()->getRequiredSigners()) {
+            _result = item.first;
+            return ORACLE_SUCCESS;
+        }
     }
 
-    return ORACLE_UNKNOWN_ERROR;
+    return ORACLE_RESULT_NOT_READY;;
 
 }
 
