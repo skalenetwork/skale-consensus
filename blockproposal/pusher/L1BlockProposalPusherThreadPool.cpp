a5b64d2f79442eeebd97bb0eb914ea66cb26ad44
kladkogex <13399135+kladkogex@users.noreply.github.com>
2022-12-23 13:48:55 +0000

    BlockProposer pull for L1

---
+++ blockproposal/pusher/L1BlockProposalPusherThreadPool.cpp
@@ -0,0 +1,32 @@
+/*
+    Copyright (C) 2018-2019 SKALE Labs
+
+    This file is part of skale-consensus.
+
+    skale-consensus is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    skale-consensus is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with skale-consensus.  If not, see <https://www.gnu.org/licenses/>.
+
+    @file BlockProposalPusherThreadPool.cpp
+    @author Stan Kladko
+    @date 2018
+*/
+
+#include "SkaleCommon.h"
+#include "Log.h"
+#include "Agent.h"
+
+#include "L1BlockProposalPusherThreadPool.h"
+
+// For L1 there is only one proposer so we only need one thread
+L1BlockProposalPusherThreadPool::L1BlockProposalPusherThreadPool(Agent* _agent ) :
+                                      BlockProposalPusherThreadPool(1, _agent) {};
