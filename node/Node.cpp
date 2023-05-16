519778973c8d256b2bb7d7e8901a474672c95019
Stan Kladko <13399135+kladkogex@users.noreply.github.com>
2023-05-16 16:16:47 +0100

    774 and 775 incorrect exit

--- node/Node.cpp
+++ node/Node.cpp
@@ -524,9 +524,9 @@ void Node::doSoftAndThenHardExit() {
 
     exitOnBlockBoundaryRequested = true;
 
-    //wait until block completes EVM processing
-    while (getSchain()->isInCreateBlock()) {
-        usleep(100 * 1000);
+    // wait until block completes EVM processing
+    while ( getSchain()->isInCreateBlock() ) {
+        usleep( 100 * 1000 );
     }
 
     auto startTimeMs = Time::getCurrentTimeMs();
