8085cc5e299d007dec945466a5e61a8332d5188e
Stan Kladko <13399135+kladkogex@users.noreply.github.com>
2025-02-22 01:01:26 +0000

    862 bite

---
+++ bite/BITEClient.h
@@ -0,0 +1,14 @@
+//
+// Created by kladko on 21-02-2025.
+//
+
+#ifndef SKALED_BITECLIENT_H
+#define SKALED_BITECLIENT_H
+
+
+class BITEClient {
+
+};
+
+
+#endif //SKALED_BITECLIENT_H
---
+++ bite/BITEManager.h
@@ -0,0 +1,15 @@
+//
+// Created by kladko on 21-02-2025.
+//
+
+#ifndef SKALED_BITEMANAGER_H
+#define SKALED_BITEMANAGER_H
+
+#include "BITEClient.h"
+
+class BITEManager {
+
+};
+
+
+#endif //SKALED_BITEMANAGER_H
