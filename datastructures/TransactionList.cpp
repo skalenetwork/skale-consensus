0c9d0ef03d33a882c12510b5d10705a8df9fa328
Konstantin Kladko <13399135+kladkogex@users.noreply.github.com>
2025-04-25 19:37:20 +0100

    862 Process failed decrypted transactions

--- datastructures/TransactionList.cpp
+++ datastructures/TransactionList.cpp
@@ -157,19 +157,9 @@ ptr<map<uint64_t, shared_ptr<vector<uint8_t> > > > _decryptedTransactions
     // for now we skip transactions for which decryption failed
     // later we will penalize the user
 
-#ifdef BITE
-    for (uint64_t i = 0; i < transactions->size(); ++i) {
-        auto it = _decryptedTransactions->find(i);
-        if (it != _decryptedTransactions->end() && !it->second) {
-            continue; // decryption failed skip from the block
-        }
-        tv->push_back(*transactions->at(i)->getData());
-    }
-#else
     for ( auto&& t : *transactions ) {
         tv->push_back( *( t->getData() ) );
     }
-#endif
     return tv;
 }
 ptr< TransactionList > TransactionList::deserialize(
