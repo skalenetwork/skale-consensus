aded8fbb8e89fd6aaeff82a17d98fa51769e3810
Oleh Nikolaiev <oleg@skalelabs.com>
2025-06-10 20:00:17 +0100

    fix toField

--- rlp/ParsedEthTransaction.cpp
+++ rlp/ParsedEthTransaction.cpp
@@ -216,7 +216,7 @@ void ParsedEthTransaction::validateToField() {
     auto toFieldIdx = 3 + type;
 
     const auto& toField = fields.at( toFieldIdx );
-    CHECK_STATE2( !toField.empty() && toField.size() == 20, "Invalid 'to' address length");
+    CHECK_STATE2( toField.empty() || toField.size() == 20, "Invalid 'to' address length");
 }
 
 inline bool ParsedEthTransaction::isZero( const std::vector< uint8_t >& _data ) {
